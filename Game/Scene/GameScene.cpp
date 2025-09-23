6ae4172ffad6f86f89b55d1d2bfc58ec4a5f255a
Clément-Alexis Fournier <clement-alexis.labrousse-fournier@epitech.eu>
2025-09-23 02:13:46 +0200

    Delete (Game/): create, use and render ennemis

--- Game/Scene/GameScene.cpp
+++ Game/Scene/GameScene.cpp
@@ -1,215 +1,176 @@
+/*
+** EPITECH PROJECT, 2025
+** G-CPP-500-PAR-5-1-rtype-1
+** File description:
+** GameScene
+*/
+
 #include "GameScene.hpp"
 #include "../Entities/Include/player.hpp"
-#include "../Entities/Include/enemy.hpp"
 #include "../Entities/Include/obstacle.hpp"
 #include "../Entities/Include/hitbox.hpp"
 #include "../Entities/Include/background.hpp"
 #include "../Entities/Include/sound.hpp"
 #include "../Entities/Include/text.hpp"
 #include "../Entities/Include/random_element.hpp"
-#include <random>
-#include <algorithm>
 #include <iostream>
 
+
 namespace game::scene {
-	GameScene::GameScene(Game &game) : AScene(800, 600, "R-Type"), _player(ecs::entity_t{0}), _game(game),
-										_enemy_spawn_interval(2.0f) {
-		_raylib = Raylib();
-		_game_running = true;
-		_enemy_spawn_timer = 0.f;
-		_startTime = 0.f;
-	}
+    GameScene::GameScene(Game &game)
+        : AScene(800, 600, "R-Type"), _player(ecs::entity_t{0}), _game(game) {
+        _raylib = Raylib();
+        _game_running = true;
+        _startTime = 0.f;
+    }
 
-	void GameScene::init() {
-		_isOpen = true;
-		_startTime = _raylib.getTime();
-		_raylib.enableCursor();
-		_raylib.setTargetFPS(60);
-
-		_registry.register_component<component::position>();
-		_registry.register_component<component::velocity>();
-		_registry.register_component<component::drawable>();
-		_registry.register_component<component::controllable>();
-		_registry.register_component<component::health>();
-		_registry.register_component<component::damage>();
+    void GameScene::init() {
+        _isOpen = true;
+        _startTime = _raylib.getTime();
+        _raylib.enableCursor();
+        _raylib.setTargetFPS(60);
+        _registry.register_component<component::position>();
+        _registry.register_component<component::velocity>();
+        _registry.register_component<component::drawable>();
+        _registry.register_component<component::controllable>();
+        _registry.register_component<component::health>();
+        _registry.register_component<component::damage>();
         _registry.register_component<component::type>();
-		_registry.register_component<component::collision_box>();
-		_registry.register_component<component::hitbox_link>();
+        _registry.register_component<component::collision_box>();
+        _registry.register_component<component::hitbox_link>();
         _registry.register_component<component::sprite>();
         _registry.register_component<component::audio>();
         _registry.register_component<component::text>();
         _registry.register_component<component::font>();
 
-		setup_movement_system();
-		setup_render_system();
-		setup_collision_system();
-		setup_health_system();
-
-		game::entities::setup_player_control_system(_registry);
-		game::entities::setup_player_bounds_system(_registry, static_cast<float>(_width), static_cast<float>(_height));
+        setup_movement_system();
+        setup_render_system();
+        setup_collision_system();
+        setup_health_system();
+        game::entities::setup_player_control_system(_registry);
+        game::entities::setup_player_bounds_system(_registry, static_cast<float>(_width), static_cast<float>(_height));
 
         create_player();
-        // Example background and music
         game::entities::create_background(_registry, 400.f, 300.f, 800.f, 600.f, "assets/background.png", 1.f);
         game::entities::create_sound(_registry, "assets/music.ogg", 0.5f, true, true);
         game::entities::create_text(_registry, 20.f, 30.f, "R-Type", 32, "assets/fonts/PressStart2P.ttf");
-        // Example random element
         game::entities::create_random_element(_registry, 600.f, 450.f, 64.f, 64.f, "assets/items/star.png", "assets/sfx/pickup.wav", 0.8f, false, false);
-		create_obstacles();
+        create_obstacles();
+        game::entities::setup_hitbox_sync_system(_registry);
+    }
 
-		// Sync hitboxes with owners
-		game::entities::setup_hitbox_sync_system(_registry);
-	}
+    void GameScene::update() {
+        if (!_game_running) return;
+        _registry.run_systems();
+        check_collisions();
+    }
 
-	void GameScene::update() {
-		if (!_game_running) return;
+    void GameScene::render() {
+        std::unordered_map<uint32_t, std::pair<float, float>> pls;
+        std::unordered_map<uint32_t, std::tuple<float, float, float, float>> obs;
+        {
+            std::lock_guard<std::mutex> g(_game.getGameClient().stateMutex);
+            pls = _game.getGameClient().players;
+            obs = _game.getGameClient().obstacles;
+        }
 
-		double delta_time = _startTime - _raylib.getTime();
-		_enemy_spawn_timer += delta_time;
-		if (_enemy_spawn_timer >= _enemy_spawn_interval) {
-			create_enemies();
-			_enemy_spawn_timer = 0.f;
-		}
+        _raylib.beginDrawing();
+        _raylib.clearBackground(BLACK);
 
-		_registry.run_systems();
+        // Obstacles from server
+        for (auto const &kv: obs) {
+            float x = std::get<0>(kv.second);
+            float y = std::get<1>(kv.second);
+            float w = std::get<2>(kv.second);
+            float h = std::get<3>(kv.second);
+            _raylib.drawRectangle((int)(x - w / 2), (int)(y - h / 2), (int)w, (int)h, GRAY);
+        }
 
-		check_collisions();
-	}
+        // Players with per-id color
+        auto colorForId = [](uint32_t id) -> Color {
+            static Color palette[] = {RAYWHITE, BLUE, GREEN, YELLOW, ORANGE, PURPLE, PINK, GOLD, LIME, SKYBLUE};
+            return palette[id % (sizeof(palette) / sizeof(palette[0]))];
+        };
+        for (auto const &kv: pls) {
+            _raylib.drawRectangle((int)(kv.second.first - 15), (int)(kv.second.second - 15), 30, 30, colorForId(kv.first));
+        }
+        _raylib.endDrawing();
+    }
 
-	void GameScene::render() {
-		std::unordered_map<uint32_t, std::pair<float, float> > pls;
-		std::unordered_map<uint32_t, std::pair<float, float> > ens;
-		std::unordered_map<uint32_t, std::tuple<float, float, float, float> > obs; {
-			std::lock_guard<std::mutex> g(_game.getGameClient().stateMutex);
-			pls = _game.getGameClient().players;
-			ens = _game.getGameClient().enemies;
-			obs = _game.getGameClient().obstacles;
-		}
-		_raylib.beginDrawing();
-		_raylib.clearBackground(BLACK);
-		// Obstacles from server
-		for (auto const &kv: obs) {
-			float x = std::get<0>(kv.second);
-			float y = std::get<1>(kv.second);
-			float w = std::get<2>(kv.second);
-			float h = std::get<3>(kv.second);
-			_raylib.drawRectangle((int) (x - w / 2), (int) (y - h / 2), (int) w, (int) h, GRAY);
-		}
-		// Players with per-id color
-		auto colorForId = [](uint32_t id) -> Color {
-			static Color palette[] = {RAYWHITE, BLUE, GREEN, YELLOW, ORANGE, PURPLE, PINK, GOLD, LIME, SKYBLUE};
-			return palette[id % (sizeof(palette) / sizeof(palette[0]))];
-		};
-		for (auto const &kv: pls) {
-			_raylib.drawRectangle((int) (kv.second.first - 15), (int) (kv.second.second - 15), 30, 30, colorForId(kv.first));
-		}
-		for (auto const &kv: ens) {
-			_raylib.drawRectangle((int) (kv.second.first - 20), (int) (kv.second.second - 20), 40, 40, RED);
-		}
-		_raylib.endDrawing();
-	}
+    void GameScene::handleEvents() {
+        update();
+        float input_x = 0.f;
+        float input_y = 0.f;
+        if (_raylib.isKeyDown(KEY_W) || _raylib.isKeyDown(KEY_UP)) input_y = -1.f;
+        if (_raylib.isKeyDown(KEY_S) || _raylib.isKeyDown(KEY_DOWN)) input_y = 1.f;
+        if (_raylib.isKeyDown(KEY_A) || _raylib.isKeyDown(KEY_LEFT)) input_x = -1.f;
+        if (_raylib.isKeyDown(KEY_D) || _raylib.isKeyDown(KEY_RIGHT)) input_x = 1.f;
+        handle_input(input_x, input_y);
+    }
 
-	void GameScene::handleEvents() {
-		update();
+    void GameScene::onClose() {
+        _game_running = false;
+    }
 
-		switch (_raylib.getKeyPressed()) {
-			default:
-				break;
-		}
+    void GameScene::handle_input(float input_x, float input_y) {
+        auto &velocities = _registry.get_components<component::velocity>();
+        auto &controllables = _registry.get_components<component::controllable>();
+        if (_player.value() < velocities.size() && velocities[_player.value()] &&
+            _player.value() < controllables.size() && controllables[_player.value()]) {
+            velocities[_player.value()]->vx = input_x * controllables[_player.value()]->speed;
+            velocities[_player.value()]->vy = input_y * controllables[_player.value()]->speed;
+        }
+        _game.getGameClient().sendInput(input_x, input_y);
+    }
 
-		// Player movements
-		float input_x = 0.f;
-		float input_y = 0.f;
+    void GameScene::setup_movement_system() {
+        _registry.add_system<component::position, component::velocity>(
+            [](ecs::registry &reg,
+               ecs::sparse_array<component::position> &pos,
+               ecs::sparse_array<component::velocity> &vel) {
+                float dt = 0.016f;
+                for (std::size_t i = 0; i < pos.size() && i < vel.size(); ++i) {
+                    if (pos[i] && vel[i]) {
+                        pos[i]->x += vel[i]->vx * dt;
+                        pos[i]->y += vel[i]->vy * dt;
+                    }
+                }
+            });
+    }
 
-		if (_raylib.isKeyDown(KEY_W) || _raylib.isKeyDown(KEY_UP)) input_y = -1.f;
-		if (_raylib.isKeyDown(KEY_S) || _raylib.isKeyDown(KEY_DOWN)) input_y = 1.f;
-		if (_raylib.isKeyDown(KEY_A) || _raylib.isKeyDown(KEY_LEFT)) input_x = -1.f;
-		if (_raylib.isKeyDown(KEY_D) || _raylib.isKeyDown(KEY_RIGHT)) input_x = 1.f;
+    void GameScene::setup_render_system() {
+        _registry.add_system<component::position, component::drawable>(
+            [this](ecs::registry &reg,
+                   ecs::sparse_array<component::position> &pos,
+                   ecs::sparse_array<component::drawable> &drw) {
+            });
+    }
 
-		handle_input(input_x, input_y);
-	}
+    void GameScene::setup_collision_system() {}
+    void GameScene::setup_health_system() {
+        _registry.add_system<component::health, component::type>(
+            [](ecs::registry &reg,
+               ecs::sparse_array<component::health> &health,
+               ecs::sparse_array<component::type> &type) {
+                for (std::size_t i = 0; i < health.size() && i < type.size(); ++i) {
+                    if (health[i] && type[i] && health[i]->current <= 0) {
+                        ecs::entity_t entity = reg.entity_from_index(i);
+                        reg.kill_entity(entity);
+                    }
+                }
+            });
+    }
 
-	void GameScene::onClose() {
-		_game_running = false;
-	}
+    void GameScene::create_player() {
+        _player = game::entities::create_player(_registry, 100.f, 300.f);
+    }
 
-	void GameScene::handle_input(float input_x, float input_y) {
-		auto &velocities = _registry.get_components<component::velocity>();
-		auto &controllables = _registry.get_components<component::controllable>();
+    void GameScene::create_obstacles() {
+        for (int i = 0; i < 3; ++i) {
+            auto obstacle = game::entities::create_obstacle(_registry, 200.f + i * 200.f, 400.f);
+            _obstacles.push_back(obstacle);
+        }
+    }
 
-		if (_player.value() < velocities.size() && velocities[_player.value()] &&
-			_player.value() < controllables.size() && controllables[_player.value()]) {
-			velocities[_player.value()]->vx = input_x * controllables[_player.value()]->speed;
-			velocities[_player.value()]->vy = input_y * controllables[_player.value()]->speed;
-		}
-		_game.getGameClient().sendInput(input_x, input_y);
-	}
-
-	void GameScene::setup_movement_system() {
-		_registry.add_system<component::position, component::velocity>(
-			[](ecs::registry &reg,
-				ecs::sparse_array<component::position> &pos,
-				ecs::sparse_array<component::velocity> &vel) {
-				float dt = 0.016f;
-
-				for (std::size_t i = 0; i < pos.size() && i < vel.size(); ++i) {
-					if (pos[i] && vel[i]) {
-						pos[i]->x += vel[i]->vx * dt;
-						pos[i]->y += vel[i]->vy * dt;
-					}
-				}
-			});
-	}
-
-	void GameScene::setup_render_system() {
-		_registry.add_system<component::position, component::drawable>(
-			[this](ecs::registry &reg,
-					ecs::sparse_array<component::position> &pos,
-					ecs::sparse_array<component::drawable> &drw) {
-			});
-	}
-
-	void GameScene::setup_collision_system() {
-	}
-
-	void GameScene::setup_health_system() {
-		_registry.add_system<component::health, component::type>(
-			[](ecs::registry &reg,
-				ecs::sparse_array<component::health> &health,
-				ecs::sparse_array<component::type> &type) {
-				for (std::size_t i = 0; i < health.size() && i < type.size(); ++i) {
-					if (health[i] && type[i] && health[i]->current <= 0) {
-						ecs::entity_t entity = reg.entity_from_index(i);
-						reg.kill_entity(entity);
-					}
-				}
-			});
-	}
-
-	void GameScene::create_player() {
-		_player = game::entities::create_player(_registry, 100.f, 300.f);
-	}
-
-	/* ----- METTRE CA DANS UN ENEMY.CPP???? (COMME PLAYER) -------- */
-	void GameScene::create_enemies() {
-		static std::random_device rd;
-		static std::mt19937 gen(rd());
-		static std::uniform_real_distribution<float> y_dist(50.f, 550.f);
-
-		auto enemy = game::entities::create_enemy(_registry, static_cast<float>(_width - 50), y_dist(gen));
-		_enemies.push_back(enemy);
-	}
-
-	/* ----- METTRE CA DANS UN OBSTACLES.CPP???? (COMME PLAYER) -------- */
-
-	void GameScene::create_obstacles() {
-		for (int i = 0; i < 3; ++i) {
-			auto obstacle = game::entities::create_obstacle(_registry, 200.f + i * 200.f, 400.f);
-			_obstacles.push_back(obstacle);
-		}
-	}
-
-	void GameScene::check_collisions() {
-		// Implémentation basique des collisions
-	}
+    void GameScene::check_collisions() {}
 } // namespace game::scene
--- Game/Scene/GameScene.hpp
+++ Game/Scene/GameScene.hpp
@@ -4,7 +4,6 @@
 ** File description:
 ** GameScene
 */
-
 #ifndef GAMESCENE_HPP
 #define GAMESCENE_HPP
 
@@ -13,129 +12,113 @@
 #include "../Game.hpp"
 
 namespace game::scene {
-	/**
-	* @class GameScene
-	* @brief Represents the main game scene for R-Type.
-	*
-	* This scene manages the game loop logic, including:
-	* - Initialization and rendering.
-	* - Handling player input and events.
-	* - Managing entities (player, enemies, obstacles).
-	* - Setting up systems (movement, rendering, collision, health).
-	* - Collision detection and game state updates.
-	*/
-	class GameScene : public AScene {
-	public:
-		/**
-		* @brief Construct a GameScene with a reference to the SceneHandler.
-		* @param game Reference to the game instance.
-		*/
-		GameScene(Game &game);
+    /**
+     * @class GameScene
+     * @brief Represents the main game scene for R-Type.
+     *
+     * This scene manages the game loop logic, including:
+     * - Initialization and rendering.
+     * - Handling player input and events.
+     * - Managing entities (player, obstacles).
+     * - Setting up systems (movement, rendering, collision, health).
+     * - Collision detection and game state updates.
+     */
+    class GameScene : public AScene {
+    public:
+        /**
+         * @brief Construct a GameScene with a reference to the Game instance.
+         * @param game Reference to the game instance.
+         */
+        GameScene(Game &game);
 
-		/**
-		* @brief Default destructor.
-		*/
-		~GameScene() override = default;
+        /**
+         * @brief Default destructor.
+         */
+        ~GameScene() override = default;
 
-		// --- Overridden lifecycle methods ---
+        // --- Overridden lifecycle methods ---
+        /**
+         * @brief Initialize the scene (entities, systems, state).
+         */
+        void init() override;
 
-		/**
-		* @brief Initialize the scene (entities, systems, state).
-		*/
-		void init() override;
+        /**
+         * @brief Render the scene (draw entities/components).
+         */
+        void render() override;
 
-		/**
-		* @brief Render the scene (draw entities/components).
-		*/
-		void render() override;
+        /**
+         * @brief Handle input and system events.
+         */
+        void handleEvents() override;
 
-		/**
-		* @brief Handle input and system events.
-		*/
-		void handleEvents() override;
+        /**
+         * @brief Called when the scene is closed.
+         */
+        void onClose() override;
 
-		/**
-		* @brief Called when the scene is closed.
-		*/
-		void onClose() override;
+        // --- Event handling ---
+        /**
+         * @brief Handle player input.
+         * @param input_x Horizontal input (movement).
+         * @param input_y Vertical input (movement).
+         */
+        void handle_input(float input_x, float input_y);
 
-		// --- Event handling ---
+    private:
+        // --- Game logic ---
+        /**
+         * @brief Update the game state (called every frame).
+         */
+        void update();
 
-		/**
-		* @brief Handle player input.
-		* @param input_x Horizontal input (movement).
-		* @param input_y Vertical input (movement).
-		*/
-		void handle_input(float input_x, float input_y);
+        // --- Game systems ---
+        /**
+         * @brief Setup the movement system for entities.
+         */
+        void setup_movement_system();
 
-	private:
-		// --- Game logic ---
+        /**
+         * @brief Setup the rendering system for entities.
+         */
+        void setup_render_system();
 
-		/**
-		* @brief Update the game state (called every frame).
-		*/
-		void update();
+        /**
+         * @brief Setup the collision detection system.
+         */
+        void setup_collision_system();
 
-		// --- Game systems ---
+        /**
+         * @brief Setup the health system for entities.
+         */
+        void setup_health_system();
 
-		/**
-		* @brief Setup the movement system for entities.
-		*/
-		void setup_movement_system();
+        // --- Entity creation ---
+        /**
+         * @brief Create the player entity and initialize components.
+         */
+        void create_player();
 
-		/**
-		* @brief Setup the rendering system for entities.
-		*/
-		void setup_render_system();
+        /**
+         * @brief Create obstacles in the scene.
+         */
+        void create_obstacles();
 
-		/**
-		* @brief Setup the collision detection system.
-		*/
-		void setup_collision_system();
+        // --- Collision management ---
+        /**
+         * @brief Check and resolve collisions between entities.
+         */
+        void check_collisions();
 
-		/**
-		* @brief Setup the health system for entities.
-		*/
-		void setup_health_system();
+        // --- Entities ---
+        ecs::entity_t _player; ///< Main player entity.
+        std::vector<ecs::entity_t> _obstacles; ///< List of active obstacles.
 
-		// --- Entity creation ---
-
-		/**
-		* @brief Create the player entity and initialize components.
-		*/
-		void create_player();
-
-		/**
-		* @brief Spawn enemy entities.
-		*/
-		void create_enemies();
-
-		/**
-		* @brief Create obstacles in the scene.
-		*/
-		void create_obstacles();
-
-		// --- Collision management ---
-
-		/**
-		* @brief Check and resolve collisions between entities.
-		*/
-		void check_collisions();
-
-		// --- Entities ---
-
-		ecs::entity_t _player; ///< Main player entity.
-		std::vector<ecs::entity_t> _enemies; ///< List of active enemies.
-		std::vector<ecs::entity_t> _obstacles; ///< List of active obstacles.
-
-		// --- Game state ---
-
-		bool _game_running; ///< Indicates whether the game is running.
-		double _enemy_spawn_timer; ///< Timer tracking enemy spawn intervals.
-		double _startTime; ///< Start time of the scene.
-		const float _enemy_spawn_interval; ///< Fixed interval between enemy spawns.
-		Game &_game;
-	};
+        // --- Game state ---
+        bool _game_running; ///< Indicates whether the game is running.
+        double _startTime; ///< Start time of the scene.
+        Game &_game; ///< Reference to the game instance.
+    };
 } // namespace game::scene
 
 #endif // GAMESCENE_HPP
