f9bd6d5faa30dc5584daa256f0eacbb22c1e8e76
Mariia Semenchenko <mariia.semenchenko@epitech.eu>
2025-09-23 20:21:42 +0200

    wip (Unit_tests): testing registry works

---
+++ Unit_test/test_registry.cpp
@@ -0,0 +1,16 @@
+#include <criterion/criterion.h>
+#include "registry.hpp"
+
+Test(Registry, spawn_and_kill_entity) {
+    ecs::registry reg;
+
+    ecs::entity_t e1 = reg.spawn_entity();
+    ecs::entity_t e2 = reg.spawn_entity();
+    cr_assert_neq(e1, e2, "id must be unique");
+
+    reg.kill_entity(e1);
+
+    ecs::entity_t e3 = reg.spawn_entity();
+    cr_assert_eq((std::size_t)e1, (std::size_t)e3,
+        "killed id should be reused when spawning new entity");
+}
