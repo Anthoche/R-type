0ec77da32036ff9b2e1c0bb7477897609ae57d2f
NuggetReckt <corto.morrow@gmail.com>
2025-09-28 15:35:23 +0200

    feat (Client): Added "localhost" argument support for serverIp

--- Client/main.cpp
+++ Client/main.cpp
@@ -8,11 +8,16 @@
 #include "../Game/Game.hpp"
 
 int main(int argc, char *argv[]) {
+	std::string serverIp;
+
 	if (argc < 3) {
 		std::cerr << "Usage: " << argv[0] << " <server_ip> <client_name>" << std::endl;
 		return 1;
 	}
-	Game game(argv[1], argv[2]);
+	serverIp = argv[1];
+	if (serverIp == "localhost")
+		serverIp = "127.0.0.1";
+	Game game(serverIp, argv[2]);
 	game.run();
 	return 0;
 }
