a6128143d7d05ae4a841b08420c9ff795b66bf99
Clément-Alexis Fournier <clement-alexis.labrousse-fournier@epitech.eu>
2025-09-28 15:33:24 +0200

    fix (protocol.hpp): complete documentation

--- Shared/protocol.hpp
+++ Shared/protocol.hpp
@@ -1,95 +1,121 @@
 /*
 ** EPITECH PROJECT, 2025
-** G-CPP-500-PAR-5-1-rtype-1
+** R-Type
 ** File description:
 ** protocol
 */
+
 
 #pragma once
 
 #include <cstdint>
 #include <string>
 
-
+/**
+ * @brief Types of messages exchanged between client and server
+ */
 enum class MessageType : uint8_t {
-    ClientHello,
-    ServerAssignId,
-    GameStart,
-    ClientInput,
-    StateUpdate,
-    EnemySpawn,
-    EnemyUpdate,
-    EnemyDespawn,
-    ObstacleSpawn,
-    ObstacleDespawn
+    ClientHello,      /**< Client greeting the server */
+    ServerAssignId,   /**< Server assigns an ID to the client */
+    GameStart,        /**< Server notifies clients that the game is starting */
+    ClientInput,      /**< Client sends input for the current frame */
+    StateUpdate,      /**< Server sends updated state for a client */
+    EnemySpawn,       /**< Server spawns a new enemy */
+    EnemyUpdate,      /**< Server updates an enemy's position */
+    EnemyDespawn,     /**< Server removes an enemy */
+    ObstacleSpawn,    /**< Server spawns a new obstacle */
+    ObstacleDespawn   /**< Server removes an obstacle */
 };
 
+/**
+ * @brief Message sent by client to introduce itself to the server
+ */
 struct ClientHelloMessage {
-    MessageType type;
-    uint32_t clientId;
-    char clientName[32];
+    MessageType type;          /**< Message type (ClientHello) */
+    uint32_t clientId;         /**< ID of the client */
+    char clientName[32];       /**< Name of the client (C-string, max 31 chars + null) */
 };
 
+/**
+ * @brief Message sent by server to notify the start of the game
+ */
 struct GameStartMessage {
-    MessageType type;
-    uint32_t clientCount;
+    MessageType type;          /**< Message type (GameStart) */
+    uint32_t clientCount;      /**< Number of connected clients */
 };
 
+/**
+ * @brief Message sent by server to assign an ID to a client
+ */
 struct ServerAssignIdMessage {
-    MessageType type;
-    uint32_t clientId; // network byte order
+    MessageType type;          /**< Message type (ServerAssignId) */
+    uint32_t clientId;         /**< Assigned client ID (network byte order) */
 };
 
-// Client -> Server: input vector for this frame (floats encoded as uint32 network order)
+/**
+ * @brief Message sent by client with input for the current frame
+ */
 struct ClientInputMessage {
-    MessageType type;         // MessageType::ClientInput
-    uint32_t clientId;        // network byte order
-    uint32_t inputXBits;      // network byte order (reinterpret cast of float)
-    uint32_t inputYBits;      // network byte order (reinterpret cast of float)
+    MessageType type;          /**< Message type (ClientInput) */
+    uint32_t clientId;         /**< Client ID (network byte order) */
+    uint32_t inputXBits;       /**< X input as float bits (network byte order) */
+    uint32_t inputYBits;       /**< Y input as float bits (network byte order) */
 };
 
-// Server -> Client: authoritative state for the local player (extend later)
+/**
+ * @brief Message sent by server to update client state
+ */
 struct StateUpdateMessage {
-    MessageType type;         // MessageType::StateUpdate
-    uint32_t clientId;        // network byte order (the recipient id)
-    uint32_t posXBits;        // network byte order (reinterpret cast of float)
-    uint32_t posYBits;        // network byte order (reinterpret cast of float)
+    MessageType type;          /**< Message type (StateUpdate) */
+    uint32_t clientId;         /**< Recipient client ID (network byte order) */
+    uint32_t posXBits;         /**< X position as float bits (network byte order) */
+    uint32_t posYBits;         /**< Y position as float bits (network byte order) */
 };
 
-// Server -> Client: spawn a new enemy with id and initial position
+/**
+ * @brief Message sent by server to spawn a new enemy
+ */
 struct EnemySpawnMessage {
-    MessageType type;     // MessageType::EnemySpawn
-    uint32_t enemyId;     // network byte order
-    uint32_t posXBits;    // network byte order
-    uint32_t posYBits;    // network byte order
+    MessageType type;          /**< Message type (EnemySpawn) */
+    uint32_t enemyId;          /**< Enemy ID (network byte order) */
+    uint32_t posXBits;         /**< X position as float bits (network byte order) */
+    uint32_t posYBits;         /**< Y position as float bits (network byte order) */
 };
 
-// Server -> Client: update enemy position
+/**
+ * @brief Message sent by server to update an enemy's position
+ */
 struct EnemyUpdateMessage {
-    MessageType type;     // MessageType::EnemyUpdate
-    uint32_t enemyId;     // network byte order
-    uint32_t posXBits;    // network byte order
-    uint32_t posYBits;    // network byte order
+    MessageType type;          /**< Message type (EnemyUpdate) */
+    uint32_t enemyId;          /**< Enemy ID (network byte order) */
+    uint32_t posXBits;         /**< X position as float bits (network byte order) */
+    uint32_t posYBits;         /**< Y position as float bits (network byte order) */
 };
 
-// Server -> Client: remove enemy
+/**
+ * @brief Message sent by server to remove an enemy
+ */
 struct EnemyDespawnMessage {
-    MessageType type;     // MessageType::EnemyDespawn
-    uint32_t enemyId;     // network byte order
+    MessageType type;          /**< Message type (EnemyDespawn) */
+    uint32_t enemyId;          /**< Enemy ID to remove (network byte order) */
 };
 
-// Server -> Client: spawn a static obstacle
+/**
+ * @brief Message sent by server to spawn a static obstacle
+ */
 struct ObstacleSpawnMessage {
-    MessageType type;     // MessageType::ObstacleSpawn
-    uint32_t obstacleId;  // network byte order
-    uint32_t posXBits;    // network byte order
-    uint32_t posYBits;    // network byte order
-    uint32_t widthBits;   // network byte order
-    uint32_t heightBits;  // network byte order
+    MessageType type;          /**< Message type (ObstacleSpawn) */
+    uint32_t obstacleId;       /**< Obstacle ID (network byte order) */
+    uint32_t posXBits;         /**< X position as float bits (network byte order) */
+    uint32_t posYBits;         /**< Y position as float bits (network byte order) */
+    uint32_t widthBits;        /**< Width as float bits (network byte order) */
+    uint32_t heightBits;       /**< Height as float bits (network byte order) */
 };
 
-// Server -> Client: remove obstacle
+/**
+ * @brief Message sent by server to remove an obstacle
+ */
 struct ObstacleDespawnMessage {
-    MessageType type;     // MessageType::ObstacleDespawn
-    uint32_t obstacleId;  // network byte order
+    MessageType type;          /**< Message type (ObstacleDespawn) */
+    uint32_t obstacleId;       /**< Obstacle ID to remove (network byte order) */
 };
