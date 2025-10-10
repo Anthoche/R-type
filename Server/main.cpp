1e8a0981ba5821cb5f7f362c86ffced89150a7bf
NuggetReckt <corto.morrow@gmail.com>
2025-10-10 11:31:52 +0200

    feat (Server): Added program params checks (if port is a number and not empty) fix (Launcher): Added port in program args

--- Server/main.cpp
+++ Server/main.cpp
@@ -7,12 +7,30 @@
 
 #include "server.hpp"
 
+bool is_number(std::string const &str) {
+	std::string::const_iterator it = str.begin();
+
+	while (it != str.end()) {
+		if (!std::isdigit(*it))
+			return false;
+		++it;
+	}
+	return !str.empty();
+}
+
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
+	if (argc < 2) {
 		std::cerr << "Usage: " << argv[0] << " <server_port> " << std::endl;
 		return 1;
 	}
-    GameServer server(static_cast<uint16_t>(std::stoi(argv[1])));
-    server.run();
-    return 0;
+	std::string const port = std::string(argv[1]);
+
+	if (port == "" || !is_number(port)) {
+		std::cerr << "The port number entered is invalid! Port must not be empty and contains only numbers!" << std::endl;
+		std::cerr << "Usage: " << argv[0] << " <server_port> " << std::endl;
+		return 1;
+	}
+	GameServer server(static_cast<uint16_t>(std::stoi(port)));
+	server.run();
+	return 0;
 }
